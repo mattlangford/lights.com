e07e3b8cd05e1d2b0c3885b5c180c149557a2e0e
Matt Langford <matthew.langford95@gmail.com>
2023-10-25 23:45:02 -0400

    Add lights

--- dmx/lights.hh
+++ dmx/lights.hh
@@ -5,95 +5,91 @@
 
 struct RgbChannel {
 public:
-    RgbChannel(Channel& red, Channel& green, Channel& blue) : red_(red), green_(green), blue_(blue) { }
+    static RgbChannel create(uint16_t address, DMXController controller) {
+        return RgbChannel(
+            &controller.channel(address),
+            &controller.channel(address + 1),
+            &controller.channel(address + 2));
+    }
+
+    RgbChannel(Channel* red=nullptr, Channel* green=nullptr, Channel* blue=nullptr) :
+        red_(red), green_(green), blue_(blue) {
+    }
 
     template <typename Effect>
     void add_effect() {
-        red_.add_effect(new Effect());
-        green_.add_effect(new Effect());
-        blue_.add_effect(new Effect());
+        if (red_) { red_->add_effect(new Effect()); }
+        if (green_) { green_->add_effect(new Effect()); }
+        if (blue_) { blue_->add_effect(new Effect()); }
     }
 
     RgbEffect effect(size_t index) {
-        return RgbEffect(red_.effect(index), green_.effect(index), blue_.effect(index));
+        return RgbEffect(
+            red_ ? red_->effect(index) : nullptr,
+            green_ ? green_->effect(index) : nullptr,
+            blue_ ? blue_->effect(index) : nullptr);
     }
 
 private:
-    Channel& red_;
-    Channel& green_;
-    Channel& blue_;
+    Channel* red_ = nullptr;
+    Channel* green_ = nullptr;
+    Channel* blue_ = nullptr;
 };
 
-/*
-   uint8_t lights_aurora[kTotalSlots] = {
-     255, 255, 15, 70, 0, 0, 0,
-     255, 50, 90, 200, 0, 0, 0,
-     255, 255, 30, 50, 0, 0, 0,
-     255, 30, 50, 200, 0, 0, 0,
-     0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-   };
-*/
 
+// Not sure of the brand of this light, this is all I have to go off of:
+//   uint8_t lights_aurora[kTotalSlots] = {
+//     255, 255, 15, 70, 0, 0, 0,
+//     255, 50, 90, 200, 0, 0, 0,
+//     255, 255, 30, 50, 0, 0, 0,
+//     255, 30, 50, 200, 0, 0, 0,
+//     0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+//   };
 class KitchenLight {
 public:
     KitchenLight(uint16_t address, DMXController& controller) :
         brightness(controller.channel(address)),
-        red(controller.channel(address + 1)),
-        green(controller.channel(address + 2)),
-        blue(controller.channel(address + 3)) {
+        rgb(RgbChannel::create(address + 1, controller)) {
         controller.set_max_channel(address + 6);
     }
 
 public:
     Channel& brightness;
-    Channel& red;
-    Channel& green;
-    Channel& blue;
+    RgbChannel rgb;
 };
 
 
-/*
 class WashLightBar52 {
 public:
     static constexpr uint8_t NUM_LIGHTS = 16;
     static constexpr uint8_t NUM_CHANNELS = 52;
 
-    WashLightBar52(uint8_t address, DMXController& controller) {
+    WashLightBar52(uint16_t address, DMXController& controller) :
+        brightness(controller.channel(address++)) {
         controller.set_max_channel(address + NUM_CHANNELS);
 
-        size_t channel = 0;
-        controller.add_channel(address++, channels[channel++]);
-        controller.add_channel(address++, bonus_channels[0]);
-        for (uint8_t light = 0; light < NUM_LIGHTS; ++light) {
-            auto& red = channels[channel++];
-            auto& green = channels[channel++];
-            auto& blue = channels[channel++];
-
-            rgb[light].set_red(&red);
-            controller.add_channel(address++, red);
-            rgb[light].set_green(&green);
-            controller.add_channel(address++, green);
-            rgb[light].set_blue(&blue);
-            controller.add_channel(address++, blue);
-        }
-
-        controller.add_channel(address++, bonus_channels[1]);
-        controller.add_channel(address++, bonus_channels[2]);
-    }
-
-    void set_goal(uint8_t r, uint8_t g, uint8_t b, uint32_t duration_ms=0) {
-        for (uint8_t light = 0; light < NUM_LIGHTS; ++light) {
-            rgb[light].set_goal(r, g, b, duration_ms);
+        for (size_t light = 0; light < NUM_LIGHTS; ++light) {
+            rgb[light] = RgbChannel::create(address + light * 3, controller);
         }
     }
 
-    LinearFadeChannel& brightness() {
-        return channels[0];
-    }
-
-    LinearFadeChannel channels[3 * NUM_LIGHTS + 1];
-    LinearFadeChannel bonus_channels[3];
-    RgbChannel<LinearFadeChannel> rgb[NUM_LIGHTS];
+    Channel& brightness;
+    RgbChannel rgb[NUM_LIGHTS];
 };
 
-*/
+class WashBarLight112 {
+public:
+    static constexpr uint8_t NUM_LIGHTS = 28;
+    static constexpr uint8_t NUM_CHANNELS = 112;
+
+    WashBarLight112(uint16_t address, DMXController& controller) {
+        controller.set_max_channel(address + NUM_CHANNELS);
+
+        for (size_t light = 0; light < NUM_LIGHTS; ++light) {
+            // Skip over white
+            rgb[light] = RgbChannel::create(address + light * 4, controller);
+        }
+    }
+
+    RgbChannel rgb[NUM_LIGHTS];
+};
