02fe7bed285a8c277f94881e794cfcb1786d3fd2
Matt Langford <matthew.langford95@gmail.com>
2023-11-04 20:04:18 -0400

    BASE_CONFIG

--- dmx/dmx.ino
+++ dmx/dmx.ino
@@ -15,13 +15,29 @@ DMXController* controller;
 EffectMap effects;
 
 struct Universe {
-    WashBarLight112* bar;
+    static constexpr size_t NUM_BARS = 3;
+    WashBarLight112* bar[NUM_BARS];
 
     Universe(DMXController& controller) {
-        bar = new WashBarLight112(1, controller);
+        bar[0] = new WashBarLight112(1, controller);
+        bar[1] = new WashBarLight112(113, controller);
+        bar[2] = new WashBarLight112(225, controller);
     }
 };
 Universe* universe;
+
+const char BASE_CONFIG[] =  R"=====(
+    {"Background":{
+      "config":{
+        "red":{"depth":3,"min_freq":1,"max_freq":10,"passthrough":0},
+        "green":{"depth":3,"min_freq":1,"max_freq":10,"passthrough":0},
+        "blue":{"depth":3,"min_freq":1,"max_freq":10,"passthrough":0}},
+      "values":{
+        "red":{"min":1,"max":50},
+        "green":{"min":0,"max":0},
+        "blue":{"min":1,"max":100}}
+    }}"
+)=====";
 
 void setup() {
     Serial.begin(115200);
@@ -32,12 +48,18 @@ void setup() {
 
     auto& background = effects.add_effect<CompositeEffect<RgbEffect<CosBlend>>>("Background");
 
-    for (size_t i = 0; i < WashBarLight112::NUM_LIGHTS; ++i) {
-        auto& rgb = background.add();
-        universe->bar->red(i).add_effect(rgb.red());
-        universe->bar->green(i).add_effect(rgb.green());
-        universe->bar->blue(i).add_effect(rgb.blue());
+    for (size_t l = 0; l < Universe::NUM_BARS; ++l) {
+        for (size_t i = 0; i < WashBarLight112::NUM_LIGHTS; ++i) {
+            auto& rgb = background.add();
+            universe->bar[l]->red(i).add_effect(rgb.red());
+            universe->bar[l]->green(i).add_effect(rgb.green());
+            universe->bar[l]->blue(i).add_effect(rgb.blue());
+        }
     }
+
+    DynamicJsonDocument doc(1024);
+    deserializeJson(doc, BASE_CONFIG);
+    effects.set_json(doc.as<JsonObject>());
 }
 
 void loop() {
