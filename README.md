d36d49bb64ad7eeca5417f9220e2d498277fcc1e
Matt Langford <matthew.langford95@gmail.com>
2024-01-06 12:42:24 -0500

    Setup for vscode, update commands

--- README.md
+++ README.md
@@ -1,15 +1,22 @@
 To build and upload with the [ardunio-cli](https://arduino.github.io/arduino-cli/0.34/getting-started/) tool
 ```bash
 # Specify the board. This uses the Serial+MIDI4 option (which lets usbMIDI work)
-$ arduino-cli board attach -b teensy:avr:teensy41:usb=serialmidi4 dmx
+arduino-cli board attach -b teensy:avr:teensy41:usb=serialmidi4
 
 # Compile and upload to the given port
-arduino-cli compile -u -p /dev/tty.usbmodem101 dmx
+arduino-cli compile -u -p /dev/tty.usbmodem101
 
-# Monitor the serial output
-cat /dev/cu.usbmodem101
+# Monitor the serial output (may require a different port)
+arduino-cli monitor -p /dev/tty.usbmodem101
 ```
 
 ## Libraries
  * ArduinoJson
  * ArduinoSTL
+
+## Lights
+These are the lights I have and the associated manuals:
+ * [Missyee36](https://images-na.ssl-images-amazon.com/images/I/B1TmOuEgH2S.pdf) RGB light
+ * [Electro86](https://cdb.s3.amazonaws.com/ItemRelatedFiles/13224/electro86.pdf) (Maybe called litake?) RGB light 
+ * [Betopper](https://cdn.shopify.com/s/files/1/0084/5230/9047/files/BETOPPER_Moving_Head_Stage_Led_Light_LM70_LM70S_Standard_User_Manual.pdf?v=1630045250) Moving Light
+ * [Washbar112](https://m.media-amazon.com/images/I/C1CE5BB5CrL.pdf) Bar light (not sure of the name)
--- lights.hh
+++ lights.hh
@@ -4,17 +4,9 @@
 #include "effects.hh"
 #include "util.hh"
 
-// Not sure of the brand of this light, this is all I have to go off of:
-//   uint8_t lights_aurora[kTotalSlots] = {
-//     255, 255, 15, 70, 0, 0, 0,
-//     255, 50, 90, 200, 0, 0, 0,
-//     255, 255, 30, 50, 0, 0, 0,
-//     255, 30, 50, 200, 0, 0, 0,
-//     0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-//   };
-class KitchenLight {
+class Missyee36Light {
 public:
-    KitchenLight(uint16_t address, DMXController& controller) :
+    Missyee36Light(uint16_t address, DMXController& controller) :
         brightness(controller.channel(address)),
         red(controller.channel(address + 1)),
         green(controller.channel(address + 2)),
@@ -26,40 +18,6 @@ public:
     Channel& red;
     Channel& green;
     Channel& blue;
-};
-
-
-class WashLightBar52 {
-public:
-    static constexpr uint8_t NUM_LIGHTS = 16;
-    static constexpr uint8_t NUM_CHANNELS = 52;
-
-    WashLightBar52(uint16_t address, DMXController& controller) :
-        brightness_(controller.channel(address++)) {
-        controller.set_max_channel(address + NUM_CHANNELS);
-        address++;
-
-        for (size_t light = 0; light < NUM_LIGHTS; ++light) {
-            rgb_[light].r = &controller.channel(address + light * 3);
-            rgb_[light].g = &controller.channel(address + light * 3 + 1);
-            rgb_[light].b = &controller.channel(address + light * 3 + 2);
-        }
-    }
-
-    Channel& brightness() { return brightness_; }
-    Channel& red(uint8_t index) { return *rgb_[index].r; }
-    Channel& green(uint8_t index) { return *rgb_[index].g; }
-    Channel& blue(uint8_t index) { return *rgb_[index].b; }
-
-private:
-    Channel& brightness_;
-
-    struct Rgb {
-        Channel* r;
-        Channel* g;
-        Channel* b;
-    };
-    Rgb rgb_[NUM_LIGHTS];
 };
 
 class WashBarLight112 {
