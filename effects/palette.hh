4544574cd48ae5a8ad1e32efdcf0c13f8dcb2086
Matt Langford <matthew.langford95@gmail.com>
2024-01-29 20:28:11 -0500

    It probably fades right now

--- effects/palette.hh
+++ effects/palette.hh
@@ -4,9 +4,9 @@
 
 struct PaletteConfig {
     struct Color {
-        uint8_t r = 0;
-        uint8_t g = 0;
-        uint8_t b = 0;
+        float r = 0.0;
+        float g = 0.0;
+        float b = 0.0;
     };
 
     // Describes what should happen on a trigger
@@ -59,13 +59,35 @@ public:
 
     void trigger(uint32_t now_ms) {
         // We might use this if we need to.
-        const size_t random_index = rand() % config_.palette.size();
+        const size_t random_index = rand();
 
         for (auto& fixture : fixtures_) {
             for (size_t l = 0; l < fixture.size(); ++l) {
-                PaletteConfig::Color to_color;
+                size_t to_index = 0;
+                switch (config_.type) {
+                case PaletteConfig::TransitionType::RANDOM:
+                    to_index = random_index;
+                    break;
+                case PaletteConfig::TransitionType::STEP:
+                    to_index = index_;
+                    break;
+                case PaletteConfig::TransitionType::UNIQUE_RANDOM:
+                    to_index = rand();
+                    break;
+                }
+
+                const PaletteConfig::Color to_color = config_.palette[to_index % config_.palette.size()];
+                const uint32_t start_time = now_ms + fixture.offset(l);
+                auto& effect = fixture.effect(l);
+
+                effect.red().fade_to(to_color.r, start_time, start_time + config_.fade_time_ms);
+                effect.green().fade_to(to_color.g, start_time, start_time + config_.fade_time_ms);
+                effect.blue().fade_to(to_color.g, start_time, start_time + config_.fade_time_ms);
             }
         }
+
+        // In case we're STEPing
+        index_++;
     }
     void clear(uint32_t now_ms) {}
 
