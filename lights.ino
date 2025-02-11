f7f35a644b255b40f8e59f025dd7e5c7944e5e3b
Matt Langford <matthew.langford95@gmail.com>
2025-02-10 22:06:55 -0500

    Only bar lights

--- lights.ino
+++ lights.ino
@@ -16,33 +16,14 @@ EffectMap effects;
 struct Universe {
     LitakeLight litake[2];
     MissyeeLight missyee[4];
-    BetopperMover9Light mover;
     WashBarLight112 bar[3];
 
     Universe(DMXController& controller):
         litake{{5, controller}, {1, controller}},
         missyee{{10, controller}, {17, controller}, {31, controller}, {24, controller}},
-        mover{40, controller},
         bar{{162, controller}, {274, controller}, {50, controller}} {
 
         controller.set_max_channel(512);
-
-        // uint8_t red = 255;
-        // uint8_t green = 30;
-
-        // litake[0].red.set_value(red);
-        // litake[1].blue.set_value(red);
-        // missyee[0].red.set_value(red);
-        // missyee[1].blue.set_value(red);
-        // missyee[2].green.set_value(red);
-        // missyee[3].red.set_value(red);
-        // missyee[3].green.set_value(red);
-        // missyee[3].blue.set_value(red);
-        // mover.red.set_value(red);
-        // mover.green.set_value(green);
-        // bar[0].set_color(red, 0, 0);
-        // bar[1].set_color(0, green, 0);
-        // bar[2].set_color(red, green, 0);
     }
 };
 Universe* universe;
@@ -121,54 +102,21 @@ void setup() {
 
     universe = new Universe(*controller);
 
-    //
-    // Panning the mover light.
-    //
-    auto* pan = &effects.add_effect<CosBlend>("pan");
-    pan->set_config(CosBlendConfig{.freq={0.25}});
-    pan->set_values(40, 80); // sweep the room!
-    universe->mover.y.set_value(10);
-    universe->mover.x.add_effect(pan);
-    pan->trigger(0);
-
     auto& kick_palette_trigger = effects.add_effect<MidiTrigger<Palette>>("kick_palette", 'C', 2);
     auto& kick_palette = kick_palette_trigger.effect();
 
     auto& palette_trigger = effects.add_effect<PeriodicTrigger<Palette>>("palette", 4000);
-    palette_trigger.set_enabled(false);
+    palette_trigger.set_enabled(true);
     auto& palette = palette_trigger.effect();
     auto& solid = effects.add_effect<Solid>("rgb");
     auto& twinkle = effects.add_effect<CompositeEffect<Twinkle>>("twinkle");
 
     uint32_t offset = 0;
 
-    universe->litake[0].add_rgb_effect(kick_palette.add_effect(offset));
-    universe->litake[0].add_rgb_effect(palette.add_effect(offset));
-    universe->litake[0].add_rgb_effect(solid);
-    universe->litake[0].add_effect(twinkle.add());
-
-    offset += 100;
-    universe->litake[1].add_rgb_effect(kick_palette.add_effect(offset));
-    universe->litake[1].add_rgb_effect(palette.add_effect(offset));
-    universe->litake[1].add_rgb_effect(solid);
-    universe->litake[1].add_effect(twinkle.add());
-
-    offset += 100;
-
-    universe->missyee[0].add_rgb_effect(kick_palette.add_effect(offset));
-    universe->missyee[0].add_rgb_effect(palette.add_effect(offset));
-    universe->missyee[0].add_rgb_effect(solid);
-    universe->missyee[0].add_effect(twinkle.add());
-
-    offset += 300;
-    universe->missyee[1].add_rgb_effect(kick_palette.add_effect(offset));
-    universe->missyee[1].add_rgb_effect(palette.add_effect(offset));
-    universe->missyee[1].add_rgb_effect(solid);
-    universe->missyee[1].add_effect(twinkle.add());
-
     offset += 100;
     auto& kick_pulse_trigger = effects.add_effect<MidiTrigger<LinearPulse>>("kick_pulse", 'C', 2);
     kick_pulse_trigger.set_every_n(2);
+    kick_pulse_trigger.set_enabled(false);
     auto& kick_pulse = kick_pulse_trigger.effect();
     kick_pulse.set_config(LinearPulseConfig{
         .rise_dt_ms=10,
@@ -177,9 +125,6 @@ void setup() {
     });
     kick_pulse.set_multiply(true);
     kick_pulse.set_min(100);
-    universe->mover.add_rgb_effect(kick_palette.add_effect(offset));
-    universe->mover.add_rgb_effect(palette.add_effect(offset));
-    universe->mover.add_effect(kick_pulse);
 
     offset += 20;
 
@@ -196,53 +141,47 @@ void setup() {
         .fall_dt_ms=1000
     });
 
-    auto add_bar_light = [&](size_t index, std::map<uint8_t, LinearPulse*> lights){
+    auto& hat_trigger = effects.add_effect<MidiTrigger<LinearPulse>>("hat_pulse", 'D', 2);
+    auto& hat_pulse = hat_trigger.effect();
+    hat_pulse.set_config(LinearPulseConfig{
+        .rise_dt_ms=10,
+        .hold_dt_ms=100,
+        .fall_dt_ms=100
+    });
+    hat_trigger.set_enabled(false);
+    hat_pulse.set_min(10);
+
+    auto add_bar_light = [&](size_t index, std::map<uint8_t, LinearPulse*> kicks, std::set<uint8_t> hat){
+        universe->bar[index].add_rgb_effect(palette.add_effect(offset));
         constexpr size_t NUM_DIVISIONS = 7;
         for (size_t i = 0; i < NUM_DIVISIONS; ++i) {
             size_t start = WashBarLight112::NUM_LIGHTS / NUM_DIVISIONS * i;
             size_t end = WashBarLight112::NUM_LIGHTS / NUM_DIVISIONS * (i + 1);
 
             offset += 20;
-            auto& effect = kick_palette.add_effect(offset);
+            auto& kick = kick_palette.add_effect(offset);
             auto& light = universe->bar[index];
-            auto it = lights.find(i);
+            auto it = kicks.find(i);
             for (size_t j = start; j < end; ++j) {
-                if (it != lights.end()) {
+                if (it != kicks.end()) {
                     light.white(j).add_effect(it->second);
                 }
-                light.add_rgb_effect(j, effect);
+                if (hat.find(i) != hat.end()) {
+                    light.red(j).add_effect(&hat_pulse);
+                    light.green(j).add_effect(&hat_pulse);
+                }
+                light.add_rgb_effect(j, kick);
                 light.add_effect(j, twinkle.add());
             }
         }
-        universe->bar[index].add_rgb_effect(palette.add_effect(offset));
         universe->bar[index].add_rgb_effect(solid);
     };
 
-    add_bar_light(0, {{2, &kick_4}, {6, &kick_6}});
-    add_bar_light(1, {{4, &kick_1}, {2, &kick_3}});
-    add_bar_light(2, {{1, &kick_5}, {3, &kick_2}});
+    add_bar_light(0, {{2, &kick_4}, {6, &kick_6}}, {1});
+    add_bar_light(1, {{4, &kick_1}, {2, &kick_3}}, {});
+    add_bar_light(2, {{1, &kick_5}, {3, &kick_2}}, {});
 
     offset += 100;
-
-    auto& hat_pulse = effects.add_effect<MidiTrigger<LinearPulse>>("hat_pulse", 'D', 2).effect();
-    hat_pulse.set_config(LinearPulseConfig{
-        .rise_dt_ms=10,
-        .hold_dt_ms=100,
-        .fall_dt_ms=100
-    });
-    hat_pulse.set_min(100);
-    universe->missyee[2].add_rgb_effect(kick_palette.add_effect(offset));
-    universe->missyee[2].add_rgb_effect(palette.add_effect(offset));
-    universe->missyee[2].add_rgb_effect(solid);
-    universe->missyee[2].add_effect(twinkle.add());
-    universe->missyee[2].brightness.set_value(0);
-    universe->missyee[2].brightness.add_effect(&hat_pulse);
-
-    offset += 100;
-    universe->missyee[3].add_rgb_effect(kick_palette.add_effect(offset));
-    universe->missyee[3].add_rgb_effect(palette.add_effect(offset));
-    universe->missyee[3].add_rgb_effect(solid);
-    universe->missyee[3].add_effect(twinkle.add());
 
     PaletteConfig config;
     config.type = PaletteConfig::TransitionType::RANDOM;
@@ -315,13 +254,6 @@ void setup() {
     // Fade in on startup
     //
     auto& blank = effects.add_effect<Blank>("blank");
-    for (auto& missyee : universe->missyee) {
-        missyee.brightness.add_effect(&blank);
-    }
-    for (auto& litake : universe->litake) {
-        litake.add_effect(blank);
-    }
-    universe->mover.add_effect(blank);
     for (auto& bar : universe->bar) {
         for (size_t l = 0; l < WashBarLight112::NUM_LIGHTS; ++l) {
             bar.red(l).add_effect(&blank);
@@ -348,15 +280,18 @@ void setup() {
         }
     });
     midi_manager.add_callback(
-        [&palette_trigger, &kick_palette_trigger, w=true, n = MidiManager::MidiNote{'D', 3}.note()]
+        [&palette_trigger, &kick_palette_trigger, &hat_trigger, w=true, n = MidiManager::MidiNote{'D', 3}.note()]
         (byte channel, byte note, byte velocity, bool on) mutable {
         if (on && note == n) {
+            Serial.print("Calm mode ");
+            Serial.println(w ? "ON" : "OFF");
+
             palette_trigger.set_enabled(w);
+            hat_trigger.set_enabled(!w);
             kick_palette_trigger.set_enabled(!w);
             w = !w;
         }
     });
-
 }
 
 void loop() {
